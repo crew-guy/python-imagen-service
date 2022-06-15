4e34f26d408df2bd0f2b24973575279857e7994c
ankitsanghvi <ankit.sanghavi87@gmail.com>
2022-06-15 15:58:51 +0530

    benchmark different approaches

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A simple flask service that exposes an endpoint to take context (text, images) as input & fit it in a predefined HTML template & return an image/pdf of the resultant view as output
+A simple python script that profiles (for speed & memory), different ways to take context (text, images) as input & fit it in a predefined HTML template & return an image/pdf of the resultant view as output
--- py-img/app.py
+++ py-img/app.py
@@ -1,18 +1,47 @@
-from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from memory_profiler import profile
+import time
+from pathlib import Path
+import pdfkit
+from weasyprint import HTML
+from html2image import Html2Image
+hti = Html2Image()
 
-app = Flask(__name__)
+t0 = time.time()
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape()
+)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+template = env.get_template("hello.html")
 
-@app.route('/ankit')
-def hello_ankit():
-    return "<p> Hey ankit</p>"
+def return_html(template_name):
+    template = env.get_template(template_name)
+    return template.render(name="UPSC Prep", profile_image="sir.png")
 
+modified_template = return_html(template) 
 
+@profile()
+def use_pdfkit(html):
+    print(html)
+    pdfkit.from_string(html, 'pdfkit.pdf')
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello_name(name=None):
-    return render_template('hello.html', name=name)
+@profile()
+def use_weasyprint(html):
+    """Generate a PDF file from a string of HTML."""
+    htmldoc = HTML(string=html, base_url="")
+    print(html)
+    pdf = htmldoc.write_pdf()
+    Path('weasyprint.pdf').write_bytes(pdf)
+
+@profile()
+def use_htmltoimz(html):
+    """Generate a PDF file from a string of HTML."""
+    hti.screenshot(html_str=html, save_as='html2img.png')
+
+# use_pdfkit(modified_template)
+# use_weasyprint(modified_template)
+use_htmltoimz(modified_template)
+t1=time.time()
+total = t1-t0
+print(total)
